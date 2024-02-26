4bfe0363d2f34774d19eac0b36e0a5ac7e891b6d
alfa33333 <44150484+alfa33333@users.noreply.github.com>
2024-02-26 12:57:39 -0500

    Completed template to display marks in the app

--- app.jl
+++ app.jl
@@ -10,6 +10,19 @@ studentDF = CSV.read("./testdata/gradesWfinalSept.csv", DataFrame);
     # studentDF = CSV.read("./testdata/gradesWfinalSept.csv", DataFrame);
     @out table = DataTable(studentDF)
     @in TableSearch_dfilter = ""
+
+    @in selected_student = ""
+    @out student_info = studentDF.Student
+    @out columns_info = []
+
+    @out List_of_info = []
+
+    @onchange selected_student begin
+        if selected_student != ""
+            List_of_info = [values(studentDF[studentDF.Student .== selected_student, Not(:Student)][1,:])...]
+            columns_info = names(studentDF[:, Not(:Student)])
+        end
+    end
 end
 
 @page("/", "app.jl.html")
