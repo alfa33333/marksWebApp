a3b5d4d8c632ae23e2ac81328ad0b534ce2d5325
alfa33333 <44150484+alfa33333@users.noreply.github.com>
2024-06-14 23:55:48 -0400

    chore: Update Genie dependency and improve app.jl functionality

--- app.jl
+++ app.jl
@@ -1,21 +1,35 @@
 module App
 using GenieFramework
+using Genie
 using DataFrames
 using CSV
 @genietools
+import Genie.Renderer.Html: normal_element, register_normal_element
+register_normal_element("q__tr", context = @__MODULE__)
+register_normal_element("q__td", context = @__MODULE__)
 
-studentDF = CSV.read("./testdata/gradesWfinalSept.csv", DataFrame);
+studentDF = CSV.read("./testdata/gradesWfinalFeb.csv", DataFrame);
 
 @app begin
     # studentDF = CSV.read("./testdata/gradesWfinalSept.csv", DataFrame);
-    @out table = DataTable(studentDF)
+    @in table = DataTable(studentDF)
     @in TableSearch_dfilter = ""
+    @in Button_process = false
+
+    @onbutton Button_process begin
+        insertcols!(studentDF, :z => 1)
+        table = DataTable(studentDF)
+    end
 
     @in selected_student = ""
     @out student_info = studentDF.Student
     @out columns_info = []
 
     @out List_of_info = []
+
+    @onchange table begin
+        # CSV.write("./testdata/gradesWfinaltest.csv",table.data)
+    end
 
     @onchange selected_student begin
         if selected_student != ""
